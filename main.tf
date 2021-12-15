ba0a50ca9fcc3c9bfeb52aebd991ebbe9d5005e9
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-12-15 12:51:33 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+module "internet" {
+  source = "github.com/getupcloud/terraform-module-internet?ref=main"
+}
+
+module "flux" {
+  source = "github.com/getupcloud/terraform-module-flux?ref=main"
+
+  git_repo       = var.flux_git_repo
+  manifests_path = "./clusters/${var.name}/generic/manifests"
+  wait           = var.flux_wait
+  manifests_template_vars = merge({
+    cronitor_id : module.cronitor.cronitor_id
+  }, var.manifests_template_vars)
+}
+
+module "cronitor" {
+  source = "github.com/getupcloud/terraform-module-cronitor?ref=main"
+
+  cluster_name  = var.name
+  customer_name = var.customer_name
+  suffix        = "generic"
+  tags          = []
+  api_key       = var.cronitor_api_key
+  pagerduty_key = var.cronitor_pagerduty_key
+  api_endpoint  = var.api_endpoint
+}
---
+++ variables.tf
@@ -0,0 +1,59 @@
+variable "name" {
+  description = "Cluster name"
+  type        = string
+}
+
+variable "kubeconfig_filename" {
+  description = "Kubeconfig path"
+  type        = string
+  default     = "~/.kube/config"
+}
+
+variable "get_kubeconfig_command" {
+  description = "Command to create/update kubeconfig"
+  type        = string
+  default     = "true"
+}
+
+variable "flux_git_repo" {
+  description = "GitRepository URL."
+  type        = string
+  default     = ""
+}
+
+variable "flux_wait" {
+  description = "Wait for all manifests to apply"
+  type        = bool
+  default     = true
+}
+
+variable "manifests_path" {
+  description = "Manifests dir inside GitRepository"
+  type        = string
+  default     = ""
+}
+
+variable "customer_name" {
+  description = "Customer name (Informative only)"
+  type        = string
+}
+
+variable "cronitor_api_key" {
+  description = "Cronitor API key. Leave empty to destroy"
+  type        = string
+  default     = ""
+}
+
+variable "cronitor_pagerduty_key" {
+  description = "Cronitor PagerDuty key"
+  type        = string
+  default     = ""
+}
+
+variable "manifests_template_vars" {
+  description = "Template vars for use by cluster manifests"
+  type        = any
+  default = {
+    alertmanager_pagerduty_key : ""
+  }
+}
